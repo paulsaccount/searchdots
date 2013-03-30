739a86f3bad731a2e94d04f9c4dd2aec53ceef0c
paulsaccount <paulpavlish@gmail.com>
2013-03-30 14:28:07 -0700

    trying port 3000

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var server = http.createServer(function(req, res) {
 
 
 
-server.listen(process.env.VCAP_APP_PORT || process.env.C9_PORT);
+server.listen(process.env.VCAP_APP_PORT || 3000);
 var io = require("socket.io").listen(server);
 io.set("log level", 2);
 
